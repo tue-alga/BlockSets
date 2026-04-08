6f6d9c6ff0b2767ab8979e440d1d1303e4b56547
Yvee1 <stevenvdb@live.nl>
2026-04-08 12:11:32 +0200

    Update Experiments file to latest

--- ILP/src/Experiments.java
+++ ILP/src/Experiments.java
@@ -5,10 +5,11 @@ import io.StatsRecorder;
 import model.PositionedSolution;
 import model.StatementEntityInstance;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.awt.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Experiments {
     public static void main(String[] args) {
@@ -22,9 +23,12 @@ public class Experiments {
 
         var uniqueID = System.currentTimeMillis();
 
+        File[] files = inputFolderDir.listFiles();
+        Arrays.sort(files, Comparator.comparingLong(File::length));
+
         try {
             for (boolean useRectEulerSplit : new boolean[]{false, true}) {
-                for (var file : inputFolderDir.listFiles()) {
+                for (File file : files) {
                     var inputName = file.getName().split("\\.(?=[^\\.]+$)")[0];
                     for (var polygonType : new PolygonType[]{PolygonType.Arbitrary, PolygonType.Orthoconvex, PolygonType.Nabla, PolygonType.Gamma, PolygonType.Rectangle}) {
                         boolean[] mosaicSetsPerimeterOptions;
@@ -39,6 +43,35 @@ public class Experiments {
                                 outputName += "_perimeter";
                             }
                             if (useRectEulerSplit) {
+                                var statementFile = new FileReader(outputFolder + outputName + ".txt");
+
+                                // We are going to determine whether this was split by the BlockSets algorithm
+                                // If so, we know we need to do this too for RectEuler
+                                // If not, the solution will be the same
+                                boolean wasSplit = false;
+
+                                BufferedReader reader = new BufferedReader(statementFile);
+                                String line;
+
+                                Pattern pattern = Pattern.compile("Entity (.+?): ((?:\\(\\d+, \\d+\\)(?: - )?)+)");
+
+                                Set<String> entities = new HashSet<>();
+
+
+                                while ((line = reader.readLine()) != null) {
+                                    Matcher matcher = pattern.matcher(line);
+
+                                    if (matcher.matches()) {
+                                        String label = matcher.group(1);
+                                        if (entities.contains(label)) {
+                                            wasSplit = true;
+                                            break;
+                                        }
+                                        entities.add(label);
+                                    }
+                                }
+                                reader.close();
+                                if (!wasSplit) continue;
                                 outputName += "_rectEulerSplit";
                             }
                             var outputFile = new File(outputFolder + outputName + ".txt");
@@ -50,7 +83,7 @@ public class Experiments {
 
                             String[] runParams = {inputName, useRectEulerSplit ? "RectEuler" : "BlockSets", polygonType.name()};
                             StatsRecorder stats = new StatsRecorder(instance, runParams);
-                            Orchestrator orchestrator = new Orchestrator(5, 1.0 / 3, 120, 120);
+                            Orchestrator orchestrator = new Orchestrator(5, 1.0 / 3, 60, 60);
                             PositionedSolution finalLayout = orchestrator.runBlockSets(instance, polygonType, stats, useRectEulerSplit, mosaicSetsPerimeter);
 
                             // Write solution stats to file
